87c966650b620aebb1c16360f9a76b660beb0982
ns3zip <ns@ns3.zip>
2026-07-03 18:27:55 +0100

    Add files via upload

---
+++ PDF/unlockpdf.py
@@ -0,0 +1,17 @@
+# Unlock PDF, Created by Nadeem Shah, 2026.
+
+# You will need to install the pikepdf library as a pip install.
+
+# MORE INFORMATION ON THIS LIBRARY CAN BE FOUND HERE:
+
+# https://pypi.org/project/pikepdf/10.9.1/#description
+
+import pikepdf
+import os
+
+# Create a temp directory if does not exist.
+
+os.makedirs('temp', exist_ok=True)
+
+pdf = pikepdf.open('uourpdffile.pdf', allow_overwriting_input=True)
+pdf.save('temp/unlocked.pdf')
