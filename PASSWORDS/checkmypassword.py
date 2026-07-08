20f432881678f5d671a7b019ca4af8ffd8b6da7a
ns3zip <ns@ns3.zip>
2026-07-08 15:17:08 +0100

    Update checkmypassword.py

--- PASSWORDS/checkmypassword.py
+++ PASSWORDS/checkmypassword.py
@@ -1,7 +1,7 @@
 #You will not be able to run this file here and will need to copy it onto your computer and run it on your machine.
 #You will also need to make sure you have installed the requests module from PyPi (pip install)
 
-# Code written by Nadeem Shah.
+# Code written by Nadeem Shah, using open source libraries.
 
 import requests
 import hashlib
